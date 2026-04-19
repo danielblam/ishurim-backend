793e38437fb60ced0ec71335b77d55733a5e7fee
Daniel Blam <danielblam29@gmail.com>
2026-04-19 11:52:44 +0300

    Moved connection string to appsettings.json

--- Services/DbService.cs
+++ Services/DbService.cs
@@ -2,7 +2,12 @@
 {
     public class DbService
     {
-        public string connectionString = @"Data Source = localhost; Initial Catalog = Ishurim; Integrated Security = True;Encrypt=false";
+        public string connectionString;
         public string salt = "rFsAaP7Dz5yvetv7oqleJ0ynkqyYdica";
+
+        public DbService(IConfiguration config)
+        {
+            connectionString = config.GetConnectionString("DefaultConnection");
+        }
     }
 }
